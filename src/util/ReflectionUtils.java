d34eecf68c7f94e045fadea9ed6d6a6035fb492b
Fernando Freitas <freitax.li@gmail.com>
2024-10-05 11:18:25 -0300

    criar classe e método útil para uso da Reflection de atributos de uma classe com superclasse

---
+++ src/util/ReflectionUtils.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Coleção de métodos úteis para uso da Reflection API
+ * 
+ * @author Fernando Freitas
+ */
+public class ReflectionUtils {
+    /**
+     * Obtém todos os atributos de uma classe que possui apenas uma superclasse
+     * 
+     * @param <T> O tipo da classe do objeto
+     * @param cls A classe
+     * @return A lista de todos os atributos juntos
+     */
+    public static <T> Field[] getAllFields(Class<T> cls) {
+        List<Field> fieldsClass = Arrays.asList(cls.getDeclaredFields());
+        List<Field> fieldsSuperClass = Arrays.asList(cls.getSuperclass().getDeclaredFields());
+
+        return (Field[]) Stream.concat(fieldsClass.stream(), fieldsSuperClass.stream()).toArray();
+    }
+}
