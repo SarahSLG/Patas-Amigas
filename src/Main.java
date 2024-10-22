6b0129ab7b65f53ebb5ea3812c889a16c6d48b1a
Fernando Freitas <126774900+ferr-ffk@users.noreply.github.com>
2024-10-22 15:25:16 -0300

    adicionar opção de usuários para teste

--- src/Main.java
+++ src/Main.java
@@ -78,9 +78,21 @@ public class Main {
         menu.adicionarOpcao(listarPessoas, "Listar pessoas.");
 
         menu.adicionarOpcao(() -> {
-            Funcionario func = new Funcionario(120, null, null, 0, null);
+            Funcionario funcionario1 = new Funcionario("João da Silva", LocalDate.of(1990, 5, 15), "Masculino", "123.456.789-00", "Rua A, 123", "(11) 98765-4321", "joao@email.com", "senha123", "hashJoao", 1, new Date(), "Desenvolvedor", 5000.0f, "Tecnologia");
+            Funcionario funcionario2 = new Funcionario("Maria Oliveira", LocalDate.of(1985, 8, 22), "Feminino", "987.654.321-00", "Rua B, 456", "(11) 91234-5678", "maria@email.com", "senha456", "hashMaria", 2, new Date(), "Analista", 6000.0f, "Financeiro");
 
-            usuarios.add(func);
+            Tutor tutor1 = new Tutor("Ana Souza", LocalDate.of(1988, 3, 12), "Feminino", "123.456.789-01", "Rua C, 789", "(11) 99876-5432", "ana@email.com", "senhaAna", "hashAna", 1, 2, "Experiência com cães e gatos", "Ativo");
+            Tutor tutor2 = new Tutor("Carlos Lima", LocalDate.of(1985, 7, 25), "Masculino", "987.654.321-09", "Avenida D, 101", "(11) 91234-5678", "carlos@email.com", "senhaCarlos", "hashCarlos", 2, 3, "Resgate de animais", "Inativo");
+
+            Adotante adotante1 = new Adotante("Lucas Mendes", LocalDate.of(1992, 4, 20), "Masculino", "111.222.333-44", "Rua E, 456", "(11) 99876-5432", "lucas@email.com", "senhaLucas", "hashLucas", 1, "Cães", "Nenhuma adoção anterior", "Ativo");
+            Adotante adotante2 = new Adotante("Fernanda Ribeiro", LocalDate.of(1987, 11, 5), "Feminino", "555.666.777-88", "Avenida F, 789", "(11) 91234-5678", "fernanda@email.com", "senhaFernanda", "hashFernanda", 2, "Gatos", "Adotou 1 gato", "Inativo");
+
+            usuarios.add(funcionario1);
+            usuarios.add(funcionario2);
+            usuarios.add(tutor1);
+            usuarios.add(tutor2);
+            usuarios.add(adotante1);
+            usuarios.add(adotante2);
 
             return null;
         }, "DEBUG : ADICIONAR USUARIOS DE TESTE.");
